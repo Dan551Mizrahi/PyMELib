861a20f3c3c2e2c8da00dcdeca8b6e9449eca796
Dan551Mizrahi <mizrahid@campus.technion.ac.il>
2025-03-16 17:14:43 +0200

    Added license, changed README.md (created a structure) and changed setup.py accordingly.

--- setup.py
+++ setup.py
@@ -6,8 +6,17 @@ setup(
     version='0.1.0',
     description='First version of the PyMELib (Python Minimal Enumeration Library) library',
     author='Dan S. Mizrahi and Batya Kenig',
+    author_email="danmizrahithemiz@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=['networkx', 'typing', 'frozendict'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
 )
